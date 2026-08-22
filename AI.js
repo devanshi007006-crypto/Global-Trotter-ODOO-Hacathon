f2e663adf3bd9c917a3fb067469063367c0be1ee
parmarprachirajput10-del <parmarprachirajput10@gmail.com>
2026-08-22 12:40:15 +0530

    Update AI.js

--- AI.js
+++ AI.js
@@ -1,1 +1,70 @@
+const activities = [ 
+  // GOA
 
+  {
+    name: "Baga Beach",
+    destination: "goa",
+    category: "Beach",
+    cost: 0,
+    rating: 5
+  },
+
+  {
+    name: "Scuba Diving",
+    destination: "goa",
+    category: "Adventure",
+    cost: 2500,
+    rating: 5
+  },
+
+  {
+    name: "Fort Aguada",
+    destination: "goa",
+    category: "History",
+    cost: 500,
+    rating: 4
+  },
+
+  {
+    name: "Water Sports",
+    destination: "goa",
+    category: "Adventure",
+    cost: 2000,
+    rating: 5
+  },
+
+  {
+    name: "Goan Food Tour",
+    destination: "goa",
+    category: "Food",
+    cost: 1200,
+    rating: 5
+  },
+
+  {
+    name: "Dudhsagar Falls",
+    destination: "goa",
+    category: "Nature",
+    cost: 1800,
+    rating: 5
+  },
+
+  {
+    name: "Local Shopping",
+    destination: "goa",
+    category: "Shopping",
+    cost: 1500,
+    rating: 4
+  },
+
+  // PARIS
+  {
+    name: "Eiffel Tower",
+    destination: "paris",
+    category: "History",
+    cost: 3000,
+    rating: 5
+  },
+
+  
+  
