642c41c55ed4493c40536174f7bdc12d739c9d05
Mishva Patel <mishvapatel75@gmail.com>
2026-08-22 12:09:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function generateTrip(){
 
     let destination = document.getElementById("destination").value;
+
+    let days=document.getElementById("days").value;
+
+    let budget=document.getElementById("budget").value;
+
+    let travelers=document.getElementById("travelers").value;
 }
